23b2fbacc37b28e5f8a40c2070deffbfd95939bf
Artem Chernenko <artem.chernenko@betterme.world>
2026-02-01 16:19:35 +0200

    add homework

--- SQL/backup.sh
+++ SQL/backup.sh
@@ -2,3 +2,4 @@
 
 mysqldump -u root -p --databases SchoolDB > SchoolDB_backup.sql
 mysql -u root -p -e "DROP DATABASE IF EXISTS SchoolDB_Restore; CREATE DATABASE SchoolDB_Restore CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+sed 's/`SchoolDB`/`SchoolDB_Restore`/g' SchoolDB_backup.sql | mysql -u root -p
